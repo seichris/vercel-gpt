32335a163ec4d828ede60d4ba4187154f2cf7fba
Chris <25006584+seichris@users.noreply.github.com>
2023-06-22 18:45:55 +0800

    Update next-auth.d.ts

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,13 +1,39 @@
-import NextAuth, { DefaultSession } from 'next-auth'
+import NextAuth, { DefaultSession, User } from 'next-auth';
 
 declare module 'next-auth' {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
-  interface Session {
+  interface Session extends DefaultSession {
     user: {
-      /** The user's postal address. */
-      id: string
-    } & DefaultSession['user']
+      id: string;
+    } & User;
+  }
+
+  interface User {
+    // Add any additional properties you need for the user object
+  }
+
+  interface Callbacks {
+    signIn(user: User, account: Account, profile: any): Promise<boolean>;
+    // Add other callback functions if needed
   }
 }
+
+const options = {
+  // NextAuth.js configuration options
+  // ...
+  callbacks: {
+    async signIn(user, account, profile) {
+      // Check if the user's GitHub username is whitelisted
+      //const whitelistedUsernames = ['seichris', 'username2', 'username3'];
+      const whitelistedUsernames = ['seichris'];
+      const { username } = profile;
+      if (!whitelistedUsernames.includes(username)) {
+        return false; // Prevent sign-in for non-whitelisted users
+      }
+
+      return true; // Allow sign-in for whitelisted users
+    },
+    // Add other callback functions if needed
+  },
+};
+
+export default NextAuth(options);
